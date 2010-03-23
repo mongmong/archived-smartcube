ef4afcfb230a09002f70a78e3b7ad2fb03efb064
Chris Chou <m2chrischou@gmail.com>
2010-03-23 21:52:25 +0800

    Change cmake files

--- build/poco.cmake
+++ build/poco.cmake
@@ -15,15 +15,15 @@ find_path(POCO_LIBRARY_DIR libPoco*
     )    
     
 if(POCO_INCLUDE_DIR)
-    message(STATUS "Poco header files found at ${POCO_INCLUDE_DIR}")
+    message(STATUS "Poco header files are found at ${POCO_INCLUDE_DIR}")
 else(POCO_INCLUDE_DIR)
-    message(FATAL_ERROR "Cound not found Poco header files.")
+    message(FATAL_ERROR "Cannot find Poco header files.")
 endif(POCO_INCLUDE_DIR)
 
 if(POCO_LIBRARY_DIR)
-    message(STATUS "Poco library files found at ${POCO_LIBRARY_DIR}")
+    message(STATUS "Poco library files are found at ${POCO_LIBRARY_DIR}")
 else(POCO_LIBRARY_DIR)
-    message(FATAL_ERROR "Cound not found Poco library files.")
+    message(FATAL_ERROR "Cannot find Poco library files.")
 endif(POCO_LIBRARY_DIR)
 
 include_directories(${POCO_INCLUDE_DIR})
--- build/python.cmake
+++ build/python.cmake
@@ -3,9 +3,13 @@ FIND_PACKAGE(PythonLibs)
 if(PYTHONLIBS_FOUND)
     message(STATUS "Python header files found at ${PYTHON_INCLUDE_DIRS}")
     message(STATUS "Python library files foundat at ${PYTHON_LIBRARIES}")
-else(Boost_FOUND)
-    message(fatal_error "Could not found Python libs.")
+    
+    include_directories(${PYTHON_INCLUDE_DIRS})
+    link_directories(${PYTHON_LIBRARIES})
+else(PYTHONLIBS_FOUND)
+    message(fatal_error "Cannot find Python libs.")
+    
+    include_directories("/usr/include")
+    link_directories("/usr/lib")
 endif(PYTHONLIBS_FOUND)
 
-include_directories(${PYTHON_INCLUDE_DIRS})
-link_directories(${PYTHON_LIBRARIES})
